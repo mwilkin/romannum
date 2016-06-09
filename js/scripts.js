6ddb44db996fe290e21fc4ae50c52b1f4d6c08af
Alexander Jacks and Matt Wilkin <alexander@>
2016-06-09 10:09:11 -0700

    add readme and begin testing business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,25 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $("input#formInput").val();
 
-    $("#result").text("this works");
+    // sorting input
+    if (input != 0) {
+      $("#result").text("no ZERO");
+    }
+    else if (input > 3999) {
+      $("#result").text("error, too large to make Roman");
+    }
+    else if (input < 4000) {
+      $("#result").text("this works");
+    }
+
+    // $("#result").text("this works");
   });
 });
+
+
+
+
+// // processing input
+// for (i=0, i<=3, i++) {
+//
+// }
