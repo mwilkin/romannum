0cb3dba0747b78f8e7f27efcb2e20bcce8639035
Alexander Jacks and Matt Wilkin <alexander@>
2016-06-09 10:11:24 -0700

    improve readme and business logic which is still nonfunctional

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     var input = $("input#formInput").val();
 
     // sorting input
-    if (input != 0) {
-      $("#result").text("no ZERO");
+    if (input = 0) {
+      $("#result").text("ZERO");
     }
     else if (input > 3999) {
       $("#result").text("error, too large to make Roman");
